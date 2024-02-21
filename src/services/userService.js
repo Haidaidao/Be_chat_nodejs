3caeaf1c5880b754bd73ae7671a05ffbf1eb4e07
daodaihai <hai0944345963@gmail.com>
2024-02-21 12:23:28 +0700

    update check status

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,25 @@
 const User = require('../models/user') 
+const { 
+    validateEmail
+} = require('../services/valid')
+
 const createUserService = async (data) => {
-    let result = null
+    let newUser = new User(); 
+ 
+    newUser.username = data.username, 
+    newUser.email = data.email,
+    newUser.password= data.password
 
-    if(data.username == null || data.password == null)
-        return result 
-
-    result = await User.create(data)
-    return result
+    let checkUser = await User.findOne({username: newUser.username})
+    
+    if(checkUser==null && validateEmail(newUser.email)) {
+        await newUser.setPassword(data.password)
+        newUser.save()
+        return newUser
+    }
+    else {
+        return null
+    }
 }
 
 const updateUserService = async (data) => {
